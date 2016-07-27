987061b00bc6fcdf5b63ad05841ba1adc9e630fc
Andrey Yatsuk <hi@yatsukav.com>
2016-07-28 00:35:44 +0300

    Level18:Lesson10:b01:b02

--- src/com/javarush/test/level18/lesson10/home10/Solution.java
+++ src/com/javarush/test/level18/lesson10/home10/Solution.java
@@ -11,7 +11,42 @@ package com.javarush.test.level18.lesson10.home10;
 Закрыть потоки. Не использовать try-with-resources
 */
 
+import java.io.*;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException
+    {
+        String o1 = "Lion.avi.part1";
+        System.out.println(o1.substring(0, o1.indexOf(".part")));
+        Set<String> parts = new TreeSet<>(new Comparator<String>()
+        {
+            @Override
+            public int compare(String o1, String o2)
+            {
+                return Integer.parseInt(o1.substring(o1.indexOf(".part")+5)) - Integer.parseInt(o2.substring(o2.indexOf(".part")+5));
+            }
+        });
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+        String outFileName = fileName.substring(0, fileName.indexOf(".part"));
+        while (!fileName.equals("end")) {
+            parts.add(fileName);
+            fileName = reader.readLine();
+        }
+        reader.close();
+
+        FileOutputStream fos = new FileOutputStream(outFileName);
+        for (String part : parts) {
+            FileInputStream fis = new FileInputStream(part);
+            byte[] buffer = new byte[fis.available()];
+            fis.read(buffer);
+            fos.write(buffer);
+            fis.close();
+        }
+        fos.close();
     }
 }
