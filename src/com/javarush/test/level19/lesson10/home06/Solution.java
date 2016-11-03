742102f345ce9fa69cf9cf22b5f3bdba58a9d8e6
Andrey Yatsuk <yatsuk@megaputer.ru>
2016-11-03 20:17:31 +0300

    19:10:7,8

--- src/com/javarush/test/level19/lesson10/home06/Solution.java
+++ src/com/javarush/test/level19/lesson10/home06/Solution.java
@@ -1,5 +1,11 @@
 package com.javarush.test.level19.lesson10.home06;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,7 +15,7 @@ import java.util.Map;
 2. Считать с консоли имя файла
 3. Заменить все числа на слова используя словарь map
 4. Результат вывести на экран
-5. Закрыть потоки. Не использовать try-with-resources
+5. Закрыть потоки
 
 Пример данных:
 Это стоит 1 бакс, а вот это - 12 .
@@ -24,8 +30,50 @@ import java.util.Map;
 
 public class Solution {
     public static Map<Integer, String> map = new HashMap<Integer, String>();
+    static {
+        map.put(12,"двенадцать");
+        map.put(11,"одинадцать");
+        map.put(10,"десять");
+        map.put(9,"девять");
+        map.put(8,"восемь");
+        map.put(7,"семь");
+        map.put(6,"шесть");
+        map.put(5,"пять");
+        map.put(4,"четыре");
+        map.put(3,"три");
+        map.put(2,"два");
+        map.put(1,"один");
+        map.put(0,"ноль");
+    }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String file1 = reader.readLine();
+        reader.close();
+        String input;
+        BufferedReader in = new BufferedReader(new FileReader(file1));
+        String bodyOfFile = "";
+        ArrayList<String> fileListStrings = new ArrayList<String>();
+        while ((input = in.readLine()) != null)
+            fileListStrings.add(input);
+        in.close();
+        for (String strinOfFile : fileListStrings) {
+            String[] arrWords = strinOfFile.split(" ");
+            for (int i = 0; i < arrWords.length; i++) {
 
+                for (Map.Entry<Integer, String> entry : map.entrySet()) {
+                    try {
+                        if (Integer.parseInt(arrWords[i]) == entry.getKey())
+                            arrWords[i] = entry.getValue();
+                    } catch (NumberFormatException ignored) {
+                    }
+                }
+            }
+            for (String currentString : arrWords)
+                System.out.print(currentString + " ");
+            System.out.println();
+        }
     }
 }
+
+
