ce946a10b620ba048b3c00983733ab6f84f7198d
Andrei Iatsuk <hi@yatsukav.com>
2018-06-06 02:31:10 +0300

    added partial solved exercise for chapter 19 from "Scala for the Impatient"

--- ScalaForImpatient/src/main/scala/net/iatsuk/problems/chapter19/Exercise05.scala
+++ ScalaForImpatient/src/main/scala/net/iatsuk/problems/chapter19/Exercise05.scala
@@ -2,6 +2,7 @@ package net.iatsuk.problems.chapter19
 
 import scala.util.matching.Regex
 import scala.util.parsing.combinator.RegexParsers
+import scala.xml.{Attribute, NodeSeq, Null, Text}
 
 // TODO: not implemented yet
 object Exercise05 extends App {
@@ -14,23 +15,24 @@ object Exercise05 extends App {
     private val openBracket = "\\s*<\\s*".r
     private val closeBracket = "\\s*>\\s*".r
 
-    def attribute: Parser[String] = rep(singleQuoteAttribute | doubleQuoteAttribute) ^^ {
-      _.foldLeft("")(_ + _)
+    def attribute: Parser[List[Attribute]] = rep(
+      singleQuoteAttribute ^^ {case singleQuoteAttribute(attr, value) => Attribute(None, attr, Text(value), Null)} |
+      doubleQuoteAttribute ^^ {case doubleQuoteAttribute(attr, value) => Attribute(None, attr, Text(value), Null)}
+    )
+
+    def emptyNode: Parser[NodeSeq] = (openBracket ~> tag ~ attribute <~ """\s*/\s*>\s*""".r) <~ text ^^ {
+      case parsed if parsed._1 == "ident" => parsed._2.foldLeft(<ident/>)(_ % _)
+      case _ => NodeSeq.Empty
     }
 
-    def emptyNode: Parser[String] = (openBracket ~> tag ~ attribute <~ "\\s*/>\\s*".r) <~ text ^^ {
-      case parsed if parsed._1 == "ident" => s"<${parsed._1} ${parsed._2}/>"
-      case _ => ""
-    }
-
-    def node: Parser[String] = (openBracket ~> tag ~ attribute <~ closeBracket) <~ text into { parsed =>
-      (rep(item) ^^ { _.foldLeft("")(_ + _) }) <~ ("</" ~ parsed._1 ~ ">") ^^ {
-        case entrails if parsed._1 == "ident" => s"<${parsed._1} ${parsed._2}>$entrails</${parsed._1}>"
-        case _ => ""
+    def node: Parser[NodeSeq] = (openBracket ~> tag ~ attribute <~ closeBracket) <~ text into { parsed =>
+      (rep(item) ^^ { _.reduceLeft(_ ++ _) }) <~ ("</" ~ parsed._1 ~ ">") ^^ {
+        case entrails if parsed._1 == "ident" => parsed._2.foldLeft(<ident>{entrails}</ident>)(_ % _)
+        case _ => NodeSeq.Empty
       }
     }
 
-    val item: Parser[String] = emptyNode | node
+    val item: Parser[NodeSeq] = emptyNode | node
   }
 
   val testXml =
