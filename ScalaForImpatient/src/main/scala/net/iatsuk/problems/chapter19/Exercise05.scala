b889c455dcfc38d345986dfa03ca7c352f01a645
Andrei Iatsuk <hi@yatsukav.com>
2018-06-06 01:52:36 +0300

    added partial solved exercise for chapter 19 from "Scala for the Impatient"

--- ScalaForImpatient/src/main/scala/net/iatsuk/problems/chapter19/Exercise05.scala
+++ ScalaForImpatient/src/main/scala/net/iatsuk/problems/chapter19/Exercise05.scala
@@ -8,38 +8,39 @@ object Exercise05 extends App {
 
   class XmlParser extends RegexParsers {
     private val tag = "\\w*".r
-    private val rawText: Regex = "[\\w*\\s*]*".r
+    private val text: Regex = "[^<>]*".r
+    private val singleQuoteAttribute = """\s*(\w*)\s*=\s*'([^<>']*)'\s*""".r
+    private val doubleQuoteAttribute = """\s*(\w*)\s*=\s*"([^<>"]*)"\s*""".r
+    private val openBracket = "\\s*<\\s*".r
+    private val closeBracket = "\\s*>\\s*".r
 
-    def text: Parser[String] = (("\"" ~ rawText <~ "\"") | ("\'" ~ rawText <~ "\'")) ^^ (t => t._1 + t._2 + t._1)
-
-    def emptyNode: Parser[String] = "<" ~> tag <~ "/>" ^^ {
-      case parsedTag if parsedTag == "ident" => s"<$parsedTag/>"
-      case _ => ""
-    }
-
-    def node: Parser[String] = "<" ~> tag <~ ">" into { parsedTag =>
-      (opt(xml) ^^ { case Some(o) => o; case None => "" }) <~ ("</" ~ parsedTag ~ ">") ^^ {
-        case entrails if parsedTag == "ident" => s"<$parsedTag>$entrails</$parsedTag>"
-        case entrails => entrails
-      }
-    }
-
-    val xml: Parser[String] = rep(emptyNode | node | text) ^^ {
+    def attribute: Parser[String] = rep(singleQuoteAttribute | doubleQuoteAttribute) ^^ {
       _.foldLeft("")(_ + _)
     }
 
+    def emptyNode: Parser[String] = (openBracket ~> tag ~ attribute <~ "\\s*/>\\s*".r) <~ text ^^ {
+      case parsed if parsed._1 == "ident" => s"<${parsed._1} ${parsed._2}/>"
+      case _ => ""
+    }
+
+    def node: Parser[String] = (openBracket ~> tag ~ attribute <~ closeBracket) <~ text into { parsed =>
+      (rep(item) ^^ { _.foldLeft("")(_ + _) }) <~ ("</" ~ parsed._1 ~ ">") ^^ {
+        case entrails if parsed._1 == "ident" => s"<${parsed._1} ${parsed._2}>$entrails</${parsed._1}>"
+        case _ => ""
+      }
+    }
+
+    val item: Parser[String] = emptyNode | node
   }
 
   val testXml =
-    """
-      |<root>
-      |  <ident>" some text"</ident>
-      |  <ident>'<![CDATA[<sender>John Smith</sender>]]>'</ident>
-      |  <ident/>
-      |</root>
-    """.stripMargin
+    """<ident tag='cloud' s2="s3">hello
+      |  <ident></ident>
+      |  <root a="r"/>vg
+      |  <ident/>gv
+      |</ident>""".stripMargin
 
   val parser = new XmlParser
-  val result = parser.parseAll(parser.xml, """<root><ident>" some text"</ident><ident/><rot/></root>""")
+  val result = parser.parseAll(parser.item, testXml)
   println(result.get)
 }
