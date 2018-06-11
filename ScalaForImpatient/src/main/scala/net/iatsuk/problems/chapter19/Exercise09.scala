02b2a85ed8316e65cb6a55ad60d85446475caed7
Andrei Iatsuk <hi@yatsukav.com>
2018-06-11 05:18:54 +0300

    added exercise 9 for chapter 19 from "Scala for the Impatient"

--- ScalaForImpatient/src/main/scala/net/iatsuk/problems/chapter19/Exercise09.scala
+++ ScalaForImpatient/src/main/scala/net/iatsuk/problems/chapter19/Exercise09.scala
@@ -7,40 +7,88 @@ object Exercise09 extends App {
 
   class ExprParser extends StandardTokenParsers {
 
-    lexical.delimiters += ("{", "}", "<", ";", "=", "+", "-", "*", "(", ")")
+    sealed trait Tree {
+      def execute(): Any
+    }
+
+    case class Block(blocks: List[Tree]) extends Tree {
+      override def execute(): Any = blocks.foreach(_.execute())
+    }
+
+    case class Loop(condition: Tree, block: Tree) extends Tree {
+      override def execute(): Unit = while (condition.execute().asInstanceOf[Boolean]) block.execute()
+    }
+
+    case class Branch(condition: Tree, onTrue: Tree, onFalse: Tree) extends Tree {
+      override def execute(): Any = if (condition.execute().asInstanceOf[Boolean]) onTrue.execute() else onFalse.execute()
+    }
+
+    case class Operator(op: String, left: Any, right: Any) extends Tree {
+      override def execute(): Any = op match {
+        case "+" => left.asInstanceOf[Tree].execute().asInstanceOf[Int] + right.asInstanceOf[Tree].execute().asInstanceOf[Int]
+        case "-" => left.asInstanceOf[Tree].execute().asInstanceOf[Int] - right.asInstanceOf[Tree].execute().asInstanceOf[Int]
+        case "*" => left.asInstanceOf[Tree].execute().asInstanceOf[Int] * right.asInstanceOf[Tree].execute().asInstanceOf[Int]
+        case "/" => left.asInstanceOf[Tree].execute().asInstanceOf[Int] / right.asInstanceOf[Tree].execute().asInstanceOf[Int]
+        case "%" => left.asInstanceOf[Tree].execute().asInstanceOf[Int] % right.asInstanceOf[Tree].execute().asInstanceOf[Int]
+        case "<" => left.asInstanceOf[Tree].execute().asInstanceOf[Int] < right.asInstanceOf[Tree].execute().asInstanceOf[Int]
+        case ">" => left.asInstanceOf[Tree].execute().asInstanceOf[Int] > right.asInstanceOf[Tree].execute().asInstanceOf[Int]
+        case "<=" => left.asInstanceOf[Tree].execute().asInstanceOf[Int] <= right.asInstanceOf[Tree].execute().asInstanceOf[Int]
+        case ">=" => left.asInstanceOf[Tree].execute().asInstanceOf[Int] >= right.asInstanceOf[Tree].execute().asInstanceOf[Int]
+        case "==" => left.asInstanceOf[Tree].execute().asInstanceOf[Int] == right.asInstanceOf[Tree].execute().asInstanceOf[Int]
+        case "!=" => left.asInstanceOf[Tree].execute().asInstanceOf[Int] != right.asInstanceOf[Tree].execute().asInstanceOf[Int]
+        case "=" => left match {
+          case "out" => println(right.asInstanceOf[Tree].execute())
+          case name: String => variables(name) = right.asInstanceOf[Tree].execute().asInstanceOf[Int]
+        }
+      }
+    }
+
+    case class Number(value: Any) extends Tree {
+      override def execute(): Any = value match {
+        case i: Int => i
+        case v: String => variables.getOrElse(v, 0)
+        case _ => Unit
+      }
+    }
+
+    lexical.delimiters += ("{", "}", "<", ">", "<=", ">=", "==", "!=", ";", "=", "+", "-", "*", "/", "%", "(", ")")
     lexical.reserved += ("while", "if", "else")
 
     val variables: mutable.HashMap[String, Int] = mutable.HashMap()
 
-    def algorithm: Parser[Any] = rep1(loop | branch | condition)
+    def alg: Parser[Tree] = loop | branch | (assignment <~ ";") | ("{" ~> rep1(alg) <~ "}") ^^ Block
 
-    def loop: Parser[Any] = (keyword("while") ~ "(") ~> (condition <~ ")") ~ ("{" ~> assignment <~ "}") ^^ (t => while (t._1) t._2)
+    def loop: Parser[Tree] = ("while" ~ "(") ~> (cond <~ ")") ~ alg ^^ (t => Loop(t._1, t._2))
 
-    def branch: Parser[Any] = (keyword("if") ~ "(") ~> (condition <~ ")") ~ ("{" ~> assignment <~ "}") ~
-      opt(keyword("else") ~> ("{" ~> assignment <~ "}")) ^^ { parsed => if (parsed._1._1) parsed._1._2 else if (parsed._2.isDefined) parsed._2.get }
+    def branch: Parser[Tree] = ("if" ~ "(") ~> (cond <~ ")") ~ alg ~ opt(keyword("else") ~> alg) ^^ { t => Branch(t._1._1, t._1._2, t._2.getOrElse(Number(null))) }
 
-    def condition: Parser[Boolean] = (factor <~ "<") ~ factor ^^ (t => t._1 < t._2)
+    def assignment: Parser[Tree] = (ident <~ "=") ~ expr ^^ (t => Operator("=", t._1, t._2))
 
-    def assignment: Parser[Any] = rep((ident <~ "=") ~ (expr <~ ";") ^^ (t => t._1 match {
-      case "out" => println(t._2)
-      case _ => variables += (t._1 -> t._2)
-    }))
+    def cond: Parser[Tree] = expr ~ ("<" | ">" | "<=" | ">=" | "==" | "!=") ~ expr ^^ (t => Operator(t._1._2, t._1._1, t._2))
 
-    // TODO: assignment and condition parse to tree and and execute method
+    def expr: Parser[Tree] = term ~ rep(("+" | "-") ~ term) ^^ (t => t._2.foldLeft(t._1)((acc, p) => Operator(p._1, acc, p._2)))
 
-    def expr: Parser[Int] = term ~ rep(("+" | "-") ~ term) ^^ (r => r._2.foldLeft(r._1)((acc, t) => t._1 match {
-      case "+" => acc + t._2
-      case "-" => acc - t._2
-    }))
+    def term: Parser[Tree] = factor ~ rep(("*" | "/" | "%") ~ factor) ^^ (t => t._2.foldLeft(t._1)((acc, p) => Operator(p._1, acc, p._2)))
 
-    def term: Parser[Int] = factor ~ rep("*" ~> factor) ^^ (r => r._2.foldLeft(r._1)(_ * _))
-
-    def factor: Parser[Int] = numericLit ^^ (_.toInt) | ident ^^ (variables.getOrElse(_, 0)) | "(" ~> expr <~ ")"
+    def factor: Parser[Tree] = numericLit ^^ (n => Number(n.toInt)) | ident ^^ (Number(_)) | "(" ~> expr <~ ")"
 
     def parseAll[T](p: Parser[T], in: String): ParseResult[T] = phrase(p)(new lexical.Scanner(in))
   }
 
   val parser = new ExprParser
-  val result = parser.parseAll(parser.condition, "4 < 3")
-  if (result.successful) println(result.get)
+  val code =
+    """{
+      |  while (b < 4) b = b + 1;
+      |  out = b;
+      |  if (b != 4)
+      |    a = 25;
+      |  else {
+      |    a = b * 10;
+      |    a = a / 2;
+      |  }
+      |  out = a;
+      |  out = 76 - a * b;
+      |}""".stripMargin
+  val result = parser.parseAll(parser.alg, code)
+  if (result.successful) result.get.execute()
 }
