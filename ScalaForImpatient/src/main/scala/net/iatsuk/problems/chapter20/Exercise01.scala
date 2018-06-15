b5c7e2b033110ef420623619954604cac0070b60
Andrei Iatsuk <hi@yatsukav.com>
2018-06-16 02:03:09 +0300

    added exercise 1 for chapter 20 from "Scala for the Impatient"

--- ScalaForImpatient/src/main/scala/net/iatsuk/problems/chapter20/Exercise01.scala
+++ ScalaForImpatient/src/main/scala/net/iatsuk/problems/chapter20/Exercise01.scala
@@ -1,24 +1,19 @@
 package net.iatsuk.problems.chapter20
 
-import scala.actors.{Actor, Channel, OutputChannel}
+import scala.actors.{Actor, Channel}
 import scala.util.Random
 
 object Exercise01 extends App {
-  val startTime = System.currentTimeMillis()
-
-  val partsCount = 1
-  val numCount = 10000000
-  val step = numCount / partsCount
 
   class SumActor extends Actor {
     override def act(): Unit = {
       react {
-        case (a: Array[Long], out: OutputChannel[(Long, Int)]) => out ! (a.sum, a.length)
+        case (a: Seq[Long], out: Actor) => out ! (a.sum, a.length)
       }
     }
   }
 
-  class AggActor extends Actor {
+  class AggActor(partsCount: Int, out: Channel[Long]) extends Actor {
     private var processedParts = 0
     private var totalSum = 0L
     private var totalCount = 0
@@ -26,29 +21,30 @@ object Exercise01 extends App {
     def avg: Long = totalSum / totalCount
 
     override def act(): Unit = {
-      while (processedParts < partsCount) {
-        receive {
-          case (sum: Long, count: Int) => totalSum += sum; totalCount += count; processedParts += 1
-        }
+      react {
+        case (sum: Long, count: Int) =>
+          totalSum += sum
+          totalCount += count
+          processedParts += 1
+          if (processedParts < partsCount) act() else out ! avg
       }
     }
   }
 
-  val a = for (i <- 0 to numCount) yield Random.nextLong()
+  Actor.actor {
+    val startTime = System.currentTimeMillis()
+    val partsCount = 10
+    val numCount = 1000000
+    val step = numCount / partsCount
 
-  val aggActor = new AggActor
-  aggActor.start()
-  val c = new Channel[(Long, Int)](aggActor)
+    val a = for (i <- 0 until numCount) yield Random.nextLong()
 
-  for (i <- 1 to partsCount) {
-    val actor = new SumActor
-    actor.start()
-    actor ! (a.slice(i * step, (i + 1) * step), c)
-    actor
+    val result = new Channel[Long]
+    val aggActor = new AggActor(partsCount, result).start()
+    for (i <- 0 until partsCount) new SumActor().start() ! (a.slice(i * step, (i + 1) * step), aggActor)
+
+    result.receive {
+      case avg => printf("Avg: %d%nTime: %dms%n", avg, System.currentTimeMillis() - startTime)
+    }
   }
-
-  while (aggActor.getState != Actor.State.Terminated) {}
-  println(aggActor.avg)
-
-  println(System.currentTimeMillis() - startTime)
 }
