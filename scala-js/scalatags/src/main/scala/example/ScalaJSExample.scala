6aba97602aac6613679c0119ce40b2a8bc589087
Andrei Iatsuk <hi@yatsukav.com>
2019-03-13 19:43:07 +0300

    Scala.js scalatags additional examples

--- scala-js/scalatags/src/main/scala/example/ScalaJSExample.scala
+++ scala-js/scalatags/src/main/scala/example/ScalaJSExample.scala
@@ -1,5 +1,6 @@
 package example
 
+import org.scalajs.dom
 import org.scalajs.dom.html
 import scalatags.JsDom.all._
 
@@ -10,6 +11,7 @@ object ScalaJSExample {
 
   @JSExport
   def main(target: html.Div): Unit = {
+    // hello world
     val (f, d) = ("fox", "dog")
     target.appendChild(
       div(
@@ -18,6 +20,63 @@ object ScalaJSExample {
           "The quick brown ", b(f),
           " jumps over the lazy ", i(d), "."
         )
+      ).render
+    )
+
+    // user input
+    val box = input(
+      `type` := "text",
+      placeholder := "Type here!"
+    ).render
+
+    val output = span.render
+
+    box.onkeyup = (e: dom.Event) => {
+      output.textContent = box.value.toUpperCase
+    }
+
+    target.appendChild(
+      div(
+        h1("Capital Box!"),
+        p("Type here and have it capitalized!"),
+        div(box),
+        div(output)
+      ).render
+    )
+
+    // re-rendering
+    val listings = Seq(
+      "Apple", "Apricot", "Banana", "Cherry", "Mango", "Mangosteen", "Mandarin", "Grape", "Grapefruit", "Guava"
+    )
+
+    val box2 = input(`type` := "text", placeholder := "Type here!").render
+
+    def renderListings = ul(
+      for {
+        fruit <- listings
+        if fruit.toLowerCase.startsWith(box2.value.toLowerCase)
+      } yield { //li(fruit)
+        val (first, last) = fruit.splitAt(box2.value.length)
+        li(
+          span(backgroundColor:="yellow", first),
+          last
+        )
+      }
+    ).render
+
+    val output2 = div(renderListings).render
+
+    box2.onkeyup = (e: dom.Event) => {
+      output2.innerHTML = ""
+      output2.appendChild(renderListings)
+    }
+
+    target.appendChild(
+      div(
+        h1("Search box!"),
+        p("Type here to filter the list of things below!"),
+        div(box2),
+        output2
       ).render
     )
   }
